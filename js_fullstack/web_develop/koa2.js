d5d1f4b480e7f74ce9ebdc5e9354554863e9b5cb
soberbad <352365541@qq.com>
2017-06-12 21:48:26 +0800

    update koa

--- js_fullstack/web_develop/koa2.js
+++ js_fullstack/web_develop/koa2.js
@@ -2,10 +2,14 @@
 
 const Koa = require('koa');
 
+const body_parser = require('koa-bodyparser');
+
 const router = require('koa-router')();
 
 const app = new Koa();
 var koa2_main = function(){
+
+app.use(body_parser());
 
 app.use(async(ctx,next)=>{
     console.log(`${ctx.request.method} ${ctx.request.url}`);
@@ -23,8 +27,31 @@ app.use(async(ctx,next)=>{
         ctx.response.body = `<h1>Hello,${name}</h1>`;
     });
 
-    router.get('/',async(ctx,next)=>{
-        ctx.response.body = '<h1>Index</h1>';
+    // router.get('/',async(ctx,next)=>{
+    //     ctx.response.body = '<h1>Index</h1>';
+    // });
+
+    router.get("/",async(ctx,next)=>{
+        ctx.response.body = `
+             <h1>Index</h1>
+             <form action="/signin" method="post">
+                 <p>Name:<input name="name" value="koa"></p>
+                 <p>Password:<input name="password" type="password"></p>
+                 <p><input type="submit" value="Submit"></p>
+             </form>
+        `;
+    });
+
+    router.post('/signin',async(ctx,next)=>{
+       var name = ctx.request.body.name ||'';
+       var password = ctx.request.body.password||'';
+       console.log(`name:${name} password:${password}`);
+       if(name==='koa'&&password==='12345'){
+           ctx.response.body = `<h1>Welcome ${name}</h1>`;
+       }else{
+           ctx.response.body = `<h1>Login failed!</h1>`;
+       }
+
     });
 
     app.use(router.routes());
