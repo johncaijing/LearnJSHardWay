37136730f139b75443779507a54fb5db7d576d24
soberbad <352365541@qq.com>
2017-06-21 23:06:43 +0800

    update js mvvm

--- js_fullstack/mvvm.js
+++ js_fullstack/mvvm.js
@@ -31,3 +31,60 @@ $(document).ready(function(){
     $('#age').text(18);
 });
 
+//JS对象的改变，导致DOM结构作出对应的变化
+//由操作DOM变成更新JS对象的状态
+
+//MVVM的设计思想：关注Model的变化，让MVVM框架取自动更新DOM的状态
+
+//MVVM框架：
+//Angular,Backbone.js,Ember,Vue.js
+
+$(function(){
+  var vm = new Vue({
+      el:'#vm',
+      data:{
+          name:'Robot',
+          age:15,
+          name2:'v-text write'
+      }
+  });
+  window.vm = vm;    
+});
+
+//单向绑定
+//Model到View的绑定
+
+//在Vue中，
+//1.直接写{{name}}绑定某个属性
+//2.使用Vue的指令v-text
+
+//双向绑定
+//Mode<-->View
+
+$(function(){
+    var form = new Vue({
+        el:'#form',
+        data:{
+            email:'',
+            name:''
+        },
+        methods:{
+            register:function(){
+                alert(JSON.stringify(this.$data));
+            }
+        }
+    });
+    var cb = new Vue({
+        el:'#cb',
+        data:{
+            language:['zh']
+        }
+    });
+    window.form = form;
+    window.cb = cb;
+});
+
+
+
+
+
